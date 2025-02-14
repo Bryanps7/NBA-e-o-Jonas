71b4186ff11efa46a13d7eabcf116a4cf6292141
artur guerso <artur_guerso@estudante.sesisenai.org.br>
2025-02-14 09:54:45 -0300

    criado o arquivo index.js e ajustado as configurações pelo dev artur

--- __tests__/Cubo.tests.js
+++ __tests__/Cubo.tests.js
@@ -1,16 +1,16 @@
-import Cubo from "./model/Cubo.js"
+import Cubo from "../model/Cubo.js"
 
 describe("testes da classe Cubo", ()=>{
     test("teste da area total do cubo", ()=>{
         const aresta = 4
-        areaTotalEsperada = 6 * (aresta * aresta)
+        let areaTotalEsperada = 6 * (aresta * aresta)
         const cubo = new Cubo(aresta)
         expect(cubo.calcularAreaTotal()).toBeCloseTo(areaTotalEsperada,2)
     })
 
     test("teste do volume do cubo", ()=>{
         const aresta = 4
-        volumeEsperado = aresta * aresta * aresta
+        let volumeEsperado = aresta * aresta * aresta
         const cubo = new Cubo(aresta)
 
         expect(cubo.calcularVolume()).toBeCloseTo(volumeEsperado,2)
